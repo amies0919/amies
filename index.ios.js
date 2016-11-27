090c46be53380b5c7f7453020ce638b8ffb78a25
amies <amies.shen@gmail.com>
2016-11-27 20:08:20 +0800

    justify content space-around

--- index.ios.js
+++ index.ios.js
@@ -72,7 +72,7 @@ let amies =  React.createClass ({
            //     <Blink text='I love to blink' />
            //     <Blink text='Yes blinking is so great' />
            // </View>
-           <View style={{flex:1,flexDirection:'column',justifyContent:'center'}}>
+           <View style={{flex:1,flexDirection:'row',justifyContent:'space-around'}}>
                 <View style={{width:50,height:50,backgroundColor:'red'}} />
                 <View style={{width:50,height:50,backgroundColor:'yellow'}} />
                 <View style={{width:50,height:50,backgroundColor:'black'}} />
