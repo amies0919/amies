76f8069ef35f775bb7b43148d93499e707214715
amies <amies.shen@gmail.com>
2017-01-23 01:02:25 +0800

    learning demos

--- index.ios.js
+++ index.ios.js
@@ -10,7 +10,7 @@ import {
   ListView,
   Navigator
 } from 'react-native';
-import MyScene from './MyScene';
+import IndexPage from './components/index.js';
 let PixelRatio = require('PixelRatio');
 let pixelRatio = PixelRatio.get();
 let Dimensions = require('Dimensions');
@@ -18,102 +18,28 @@ let totalWidth = Dimensions.get('window').width;
 let totalHeight = Dimensions.get('window').height;
 let leftStartPoint = totalWidth*0.1;
 let componentWidth = totalWidth*0.8;
-class Greeting extends Component{
-    render(){
-        return (
-            <Text style={{left:50}}>Hello {this.props.name}!</Text>
-        );
-    }
-}
-class Blink extends Component {
-    constructor(props){
-        super(props);
-        this.state = {showText:true};
-        setInterval(()=>{
-            this.setState({showText:!this.state.showText});
-        },1000);
-    }
-    render(){
-        let display = this.state.showText? this.props.text:'';
-        return(
-            <Text>{display}</Text>
-        );
-    }
-}
 class amies extends Component {
-    constructor(props){
-        super(props);
-        const ds = new ListView.DataSource({rowHasChanged:(r1,r2) => r1 !== r2});
-        this.state = {
-            dataSource: ds.cloneWithRows([
-                'John','Joel','James','Jimmy','Jackson','Jillian','Julie','Devin'  
-            ])
-        };
-    }
-    render() {
-        fetch('https://facebook.github.io/react-native/movies.json').then((response) =>response.json()).then((responseJson) =>{
-            console.log(responseJson);
-        }).catch((error)=>{console.error(error)});
-        return (
-            <Navigator initialRoute={{title: 'My Initial Scene',index:0}} renderScene = {
-                (route, navigator)=> 
-                <MyScene title={route.title} onForward={()=>{
-                    const nextIndex = route.index +1;
-                    navigator.push({
-                        title:'Scene' + nextIndex,
-                        index: nextIndex
-                    });
-                }} onBack={() =>{
-                    if(route.index>0){
-                        navigator.pop();
-                    }
-                }} />
-            }/>
-        );
-    }
-};
+    render(){
+       return (
+      <Navigator
+          tintColor='#cccccc'
+          barTintColor='#cccccc'
+          initialRoute={{ name: 'indexPage', component: IndexPage }}
+          /**
+           * 切换界面方式,只能向前切换，不能向后切换
+           */
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-    },
-    numberInputStyle: {
-        top: 20,
-        left: leftStartPoint,
-        width: componentWidth,
-        backgroundColor: 'gray',
-        fontSize: 20,
-        height: 70
-    },
-    textPromptStyle: {
-        top: 30,
-        left: leftStartPoint,
-        width: componentWidth,
-        fontSize: 20
-    },
-    passwordInputStyle: {
-        top: 50,
-        left: leftStartPoint,
-        width: componentWidth,
-        backgroundColor: 'gray',
-        fontSize: 20,
-        height: 70
-    },
-    bigTextPrompt: {
-        top:70,
-        left: leftStartPoint,
-        width: componentWidth,
-        backgroundColor: 'gray',
-        color: 'white',
-        textAlign: 'center',
-        fontSize: 60
-    },
-    bigTextPrompts: {
-        top:70,
-        left: leftStartPoint,
-        width: componentWidth
+          configureScene={(route) => {
+            let  gestureType = Navigator.SceneConfigs.HorizontalSwipeJump;
+            gestureType.gestures.jumpForward=null;
+             return gestureType
+          }}
+          renderScene={(route, navigator) => {
+            let Component = route.component;
+            return <Component {...route.params} {...route} navigator={navigator} />
+          }}>
+      </Navigator>
+    );
     }
-});
-
+}
 AppRegistry.registerComponent('amies', () => amies);
